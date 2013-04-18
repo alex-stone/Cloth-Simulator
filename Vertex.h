d97b0657e246858a4753b93dbe0bba7580b0e4b1
Alex Stone <alexstone@berkeley.edu>
2013-04-18 14:03:19 -0700

    Added Vertex Header - Position, Velocity, Acceleration, Springs

---
+++ Vertex.h
@@ -0,0 +1,26 @@
+#ifndef VERTEX_H
+#define VERTEX_H
+
+//****************************************************
+// Vertex Header Definition
+//****************************************************
+
+class Vertex {
+    private:
+        // TODO: Vertex Properties:
+        // Position - Coordinate
+        // Velocity - Vector
+        // Acceleration - Vector
+
+        // TODO: Spring Connections:
+        // 4 Stretch Springs: Horizontal & Vertical
+        // 4 Shear Springs: Diagonal
+        // 4 Bend Springs: 2nd Horizontal & Vertical
+
+        // TODO: Variables
+        // Bool fixed
+        // 
+
+};
+
+#endif
