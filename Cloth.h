120e543186360685654dbc498df970030e908c7f
Alex Stone <alexstone@berkeley.edu>
2013-04-18 15:00:59 -0700

    Cloth Class Added

---
+++ Cloth.h
@@ -0,0 +1,30 @@
+#ifndef CLOTH_H
+#define CLOTH_H
+
+//****************************************************
+// Cloth Header Definition
+//****************************************************
+
+class Cloth {
+  private:
+    // Vertices:
+    // 2D array of Vertices 
+           
+    // Color: 
+
+    // Texture
+
+    // Bool Tearable
+    // Bool Wireframe
+
+  public:
+    // Constructors:
+    Cloth();
+    // TODO: Constructor Takes a list of Vertices
+    // Constructor Options:
+    // Spring Constants
+    // Stretch, Shear, Bend Constants 
+
+}
+
+#endif
