036cf45a03b9d9e99f12b3c781c0064003431002
Alex Stone <alexstone@berkeley.edu>
2013-04-18 18:56:21 -0700

    Vertex Class - TODOs added

--- Vertex.h
+++ Vertex.h
@@ -6,20 +6,31 @@
 //****************************************************
 
 class Vertex {
-    private:
-        // TODO: Vertex Properties:
-        // Position - Coordinate
-        // Velocity - Vector
-        // Acceleration - Vector
+  private:
+    // TODO: Vertex Properties:
+    // Position - Coordinate
+    // Velocity - Vector
+    // Acceleration - Vector
+    //     Force's Calculated for Each Vertex
 
-        // TODO: Spring Connections:
-        // 4 Stretch Springs: Horizontal & Vertical
-        // 4 Shear Springs: Diagonal
-        // 4 Bend Springs: 2nd Horizontal & Vertical
 
-        // TODO: Variables
-        // Bool fixed
-        // 
+    // TODO: Spring Connections:
+    // 4 Stretch Springs: Horizontal & Vertical
+    // 4 Shear Springs: Diagonal
+    // 4 Bend Springs: 2nd Horizontal & Vertical
+
+    // TODO: Variables
+    // Bool fixed
+    // Float mass
+
+  public:
+    // Constructors:
+    Vertex();
+    // Constructor Options:
+    // Point in Space
+    // Cloth Vertex Coordinate
+    // Connections 
+    // Fixed Or Not
 
 };
 
