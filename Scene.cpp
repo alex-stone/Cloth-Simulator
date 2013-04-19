eca3e167f9dc05f042b18e9e5efa8e2399b0b6b0
Alex Stone <alexstone@berkeley.edu>
2013-04-18 18:52:54 -0700

    Scene Class created - GLUT Initialized stuff added

---
+++ Scene.cpp
@@ -0,0 +1,102 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+#ifdef OSX
+#include <GLUT/glut.h>
+#include <OpenGL/glu.h>
+#else     
+#include <GL/glut.h>
+#include <GL/glu.h> 
+#endif
+             
+#include <time.h>
+#include <math.h>
+
+#define PI 3.14159265
+
+using namespace std;
+
+class Viewport {
+  public:
+    int w, h; // width and height
+};
+
+//****************************************************
+// Global Variables 
+//****************************************************
+Viewport    viewport;
+Cloth*      cloth;
+
+// OpenGL Drawing Variables
+bool wire;
+bool smooth;
+
+
+//****************************************************
+// Glut Functions
+//****************************************************
+
+void initScene() {
+
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );
+
+    // Initialize Viewport Size
+    viewport.w = 400;
+    viewport.h = 400;
+
+    // The Size and Position of the Window
+    glutInitWindowSize(viewport.w, viewport.h);
+    glutInitWindowPosition(0,0);
+    glutCreateWindow("CS184 - Final Project");
+
+
+
+}
+
+void myReshape(int w, int h) {
+    viewport.w = w;
+    viewport.h = h;
+
+}
+
+void myDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glFlush();
+    glutSwapBuffers();
+}
+
+void keyPress(unsigned char key, int x, int y) {
+
+}
+
+void loadCloth(char* input) {
+
+}
+
+int main(int argc, char *argv[]) {
+    
+    // Initialize GLUT
+    glutInit(&argc, argv);
+
+    initScene();
+
+    
+    glutDisplayFunc(myDisplay);
+    glutReshapeFunc(myReshape);
+    glutKeyboardFunc(keyPress);
+    glutMainLoop();
+
+}
+
+
+
+
