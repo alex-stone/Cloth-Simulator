ce8d7bcddf42fdb6717410887c1c782e1e8e8fca
Alex Stone <alexstone@berkeley.edu>
2013-04-23 12:18:23 -0700

    Adding Vertices to Vertex Class for each spring direction

--- Vertex.h
+++ Vertex.h
@@ -17,9 +17,15 @@ class Vertex {
 
 
     // TODO: Spring Connections:
-    // 4 Stretch Springs: Horizontal & Vertical
-    // 4 Shear Springs: Diagonal
-    // 4 Bend Springs: 2nd Horizontal & Vertical
+    // 4 Stretch Spring: Left, Up, Right, Down
+    Vertex* stretch1, stretch2, stretch3, stretch4;
+
+    // 4 Shear Springs: Up-Left, Up-Right, Down-Right, Down-Left
+    Vertex* shear1, shear2, shear3, shear4;
+    
+    // 4 Bend Springs: Left, Up, Right, Down
+    Vertex* bend1, bend2, bend3, bend4;
+
 
     // TODO: Variables
     bool fixed;
@@ -43,6 +49,9 @@ class Vertex {
     float getZ() { return z; };
 
 
+    void connectStretch(Vertex a);
+    void connectShear(Vertex a);
+    void connectBend(Vertex a);
 
 };
 
