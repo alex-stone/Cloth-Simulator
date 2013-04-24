a0f31434d3e2f83abda61481b22bc03ccaa3c7b7
Alex Stone <alexstone@berkeley.edu>
2013-04-24 05:58:25 -0700

    Set Rest lengths of springs when creating vertices

--- Cloth.cpp
+++ Cloth.cpp
@@ -41,7 +41,12 @@ Cloth::Cloth(int w, int h, Vertex* upLeft, Vertex* upRight, Vertex* downRight, V
 
     glm::vec3 vertStep = vertVec / (float)(h-1);
     glm::vec3 horizStep = horizVec / (float)(w-1);
-   
+
+    // Set Spring Rest Lengths   
+    float stretchLength = glm::length(vertStep);
+    float bendLength = 2 * stretchLength;
+    float shearLength = sqrt(2 * stretchLength * stretchLength);
+
     // Iterate through and create each Vertex
     for(int i = 0; i < h; i++) {
         for(int j = 0; j < w; j++) {
@@ -49,9 +54,11 @@ Cloth::Cloth(int w, int h, Vertex* upLeft, Vertex* upRight, Vertex* downRight, V
             
             //I*W + j indexes vertexMatrix like a 2D array vertexMatrix[i][j]; 
             vertexMatrix[i*w + j] = new Vertex(temp.x, temp.y, temp.z);
-             
+            vertexMatrix[i*w + j]->setSpringRestLengths(stretchLength, bendLength, shearLength); 
         }
     }
+
+    this->connectSprings();
 
 }
 
@@ -64,4 +71,74 @@ void Cloth::update(float timestep) {
 
 }
 
+void Cloth::connectSprings() {
+    for(int i = 0; i < width; i++) {
+        for(int j = 0; j < height; j++) {
+            Vertex* vert = this->getVertex(i, j);
+            
+            // Connections to the Left:
+            if(i >= 2) {
+                vert->connectBend(this->getVertex(i-2, j), 0);
+                vert->connectStretch(this->getVertex(i-1, j), 0);
+            } else {
+                if(i == 1) {
+                    vert->connectStretch(this->getVertex(i-1, j), 0);
+                }
+            }   
+
+            // Connections Upwards:
+            if(j >= 2) {
+                vert->connectBend(this->getVertex(i, j-2), 1);
+                vert->connectStretch(this->getVertex(i, j-1), 1);
+            } else {
+                if(j == 1) {
+                    vert->connectStretch(this->getVertex(i, j-1), 1);
+                }
+            }
+
+            // Connections to the Right
+            if(i <= width - 2) {
+                vert->connectBend(this->getVertex(i+2, j), 2);
+                vert->connectStretch(this->getVertex(i+1, j), 2);
+            } else {
+                if(i == width - 1) {
+                    vert->connectStretch(this->getVertex(i+1, j), 2);
+                }
+            }
+
+            // Connections Downwards:
+            if(j <= height - 2) {
+                vert->connectBend(this->getVertex(i, j+2), 3);
+                vert->connectStretch(this->getVertex(i, j+1), 3);   
+            } else {
+                if(j == height - 1) {
+                    vert->connectStretch(this->getVertex(i, j+1), 3);
+                }
+            }
+
+            // Diagonal Conections:
+            
+            // Connection Up-Left
+            if(i >= 1 && j >= 1) {
+                vert->connectShear(this->getVertex(i-1, j-1), 0);
+            }
+
+            // Conection Up-Right
+            if(i <= (width - 1) && j >= 1) {
+                vert->connectShear(this->getVertex(i+1, j-1), 1);
+            }
+
+            // Connection Down-Right:
+            if(i <= (width - 1) && j <= (height - 1)) {
+                vert->connectShear(this->getVertex(i+1, j+1), 2);
+            }
+
+            // Connection Down-Left:
+            if(i >= 1 && j <= (height - 1)) {
+                vert->connectShear(this->getVertex(i-1, j+1), 3);
+            }
+
+        }
+    }
+}
 
