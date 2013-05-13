de1d7c6121936f77f25f5118e2c2913908cd94d0
Alex Stone <alexstone@berkeley.edu>
2013-05-12 20:14:05 -0700

    Added Spring Class that holds 2 vertices and can apply force onto both vertices

---
+++ Spring.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include "glm/glm.hpp"
+
+#include "Spring.h"
+#include "Vertex.h"
+
+//****************************************************
+// Spring Class - Constructors
+//****************************************************
+Spring::Spring(Vertex* v1, Vertex* v2, float kconstant, std::string t) {
+	vertex1 = v1;
+	vertex2 = v2;
+
+	restDistance = glm::length(v2-v1);
+	springConstant = kconstant;
+
+	type = t;
+}
+
+//****************************************************
+// Spring Class - Magnitude of Force due to Spring
+//****************************************************
+glm::vec3 Spring::getForce() {
+	glm::vec3 v1 = vertex1->getPos();
+	glm::vec3 v2 = vertex2->getPos();
+
+	float displacement = glm::length(v2-v1);
+	glm::vec3 springVec = vertex1->vectorTo(vertex2);
+
+	return springVec * springConstant * displacement;
+}
+
+//****************************************************
+// Apply Force:
+//		For the given Position of the vertices it
+//		Adds the new forces due to springs to the
+//		Vertices
+//****************************************************
+void Spring::applyForce() {
+	glm::vec3 v1 = vertex1->getPos();
+	glm::vec3 v2 = vertex2->getPos();
+
+	glm::vec3 springVec = v2-v1;
+
+	float displacement = glm::length(springVec);
+	float magnitude = springConstant * displacement;
+
+	vertex1->addForce(springVec * magnitude);
+	vertex2->addForce(springVec * magnitude * (-1.0f));
+
+}
+
---
+++ Spring.h
@@ -0,0 +1,35 @@
+#ifndef SPRING_H
+#define SPRING_H
+
+#include "glm/glm.hpp"
+#include "Vertex.h"
+
+//****************************************************
+// Spring Header Definition
+//****************************************************
+
+class Spring {
+  private:
+  	Vertex* vertex1;
+  	Vertex* vertex2;
+
+  	// Spring Information
+  	float springConstant;
+  	float restDistance;
+
+  	// Type, "STRETCH", "SHEAR", "BEND"
+  	std::string type;
+
+  public:
+  	// Constructors
+  	Spring(Vertex* v1, Vertex* v2, float kconstant, std::string t);
+
+  	// Getters
+  	std::string getType() { return type; };
+
+  	// Force Due to Spring:
+  	glm::vec3 getForce();
+  	void applyForce();
+
+};
+#endif
