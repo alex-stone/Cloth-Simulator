0032c150a8080d61b1c1d8855d50e42a619b6ed0
Alex Stone <alexstone@berkeley.edu>
2013-05-13 18:52:39 -0700

    Split Scene into main and Scene Class

---
+++ Scene.h
@@ -0,0 +1,61 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include "glm/glm.hpp"
+#include "Cloth.h"
+#include "Shape.h"
+#include "Vertex.h"
+
+//****************************************************
+// Spring Header Definition
+//****************************************************
+
+class Scene {
+  private:
+    const char* inputFile;
+    const char* shapeFile;
+
+    // Cloth Info
+
+
+    // Shape Info
+    std::vector<Shape*> shapes;
+    int numShapes;
+
+    // Integration Method
+    bool euler;
+    bool useFloor;
+
+  public:
+    Cloth* cloth;
+
+    // Constructors
+    Scene(const char* clothFile, const char* sceneFile, bool isEuler, bool isFloor);
+
+    // Getters:
+    bool isEuler() { return euler; };
+
+    int getNumShapes() { return numShapes; };
+    Shape* getShape(int i) { return shapes[i]; };
+
+
+    // Setters:
+   // bool useFloor();  // If not using floor, if using floor Remove it
+
+
+    // Sets up new Cloth, and new Scenes
+    void resetScene(const char* newCloth, const char* newScene, bool isEuler, bool isFloor);
+  
+    // Functions to Load New Cloth / Shapes
+    void loadShapes(const char* shapeInput);
+    void loadCloth(const char* input);
+
+    // Update Collisions through each shape
+    void updateCollisions();
+
+    // Adds Floor to Shapes Vector
+    void addFloor();
+
+
+};
+#endif
